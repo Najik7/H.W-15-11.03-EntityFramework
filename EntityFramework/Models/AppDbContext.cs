48fb6ea8865e26de6a850003efadfebf27174d98
User <mm0562879@gmail.com>
2021-03-14 17:16:35 +0500

    Добавьте файлы проекта.

---
+++ EntityFramework/Models/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public AppDbContext()
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; } 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data source = WIN-C6IAG73172R\SQLEXPRESS; initial = AlifAcademy; integrated security = true;");
+        }
+    }
+}
+
+
---
+++ EntityFramework/Program.cs
@@ -0,0 +1,37 @@
+﻿using EntityFramework.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CreateModel();
+        }
+        public static async Task CreateModel()
+        {
+            var @newCustomer = new Customer();
+            Console.WriteLine("Enter your name: ");
+            newCustomer.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter your lastName: ");
+            newCustomer.LastName = Console.ReadLine();
+            Console.WriteLine("Enter your middleName: ");
+            newCustomer.MiddleName = Console.ReadLine();
+            Console.WriteLine("Enter you DateBirth (ex: yyyy-mm-dd): ");
+            newCustomer.DateOfBirth = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your DocumentNo: ");
+            newCustomer.DocumentNumber = Console.ReadLine();
+
+
+
+            using (AppDbContext context = new AppDbContext())
+            {
+                context.Add(newCustomer);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+
+}
