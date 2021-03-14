5b821d38822fbcfa8c73b6ef85ded04273a84299
User <mm0562879@gmail.com>
2021-03-14 19:01:22 +0500

    Создал Create

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Threading.Tasks;
 
@@ -8,43 +9,50 @@ namespace EntityFramework
     {
         static async Task Main(string[] args)
         {
-            using(var alifcontext = new AppDbContext())
+
+            while (true)
             {
-                var customers = alifcontext.Customers;
-                foreach (var customer in customers)
-                {
-                    Console.WriteLine($"Id: {customer.Id}\t FirstName: {customer.FirstName}\t LastName: {customer.LastName}\t MiddleName: {customer.MidlleName}\t " +
-                                      $"Date of Birth: {customer.DateOfBirth}\t DocumentNumber: {customer.DocumentNumber}");
-                }
+                Console.Clear();
+                await Read();
+                await Create();
+                Console.ReadLine();
+            }
+            
+        }
+
+        private static async Task Create()
+        {
+            var @newCustomer = new Customer();
+
+            Console.WriteLine("Enter your FirstName: ");
+            newCustomer.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter your LastName: ");
+            newCustomer.LastName = Console.ReadLine();
+            Console.WriteLine("Enter your MiddleName: ");
+            newCustomer.MidlleName = Console.ReadLine();
+            Console.WriteLine("Enter you DateBirth(ex: yyyy - mm - dd)");
+            newCustomer.DateOfBirth = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your DocumentNo: ");
+            newCustomer.DocumentNumber = Console.ReadLine();
+
+            using(var creatcnt = new AppDbContext())
+            {
+                creatcnt.Add(newCustomer);
+                await creatcnt.SaveChangesAsync();
             }
 
-
-            Console.ReadLine();
-
-            //await CreateModel();
         }
-        //public static async Task CreateModel()
-        //{
-        //    var @newCustomer = new Customer();
-        //    Console.WriteLine("Enter your name: ");
-        //    newCustomer.FirstName = Console.ReadLine();
-        //    Console.WriteLine("Enter your lastName: ");
-        //    newCustomer.LastName = Console.ReadLine();
-        //    Console.WriteLine("Enter your middleName: ");
-        //    newCustomer.MiddleName = Console.ReadLine();
-        //    Console.WriteLine("Enter you DateBirth (ex: yyyy-mm-dd): ");
-        //    newCustomer.DateOfBirth = DateTime.Parse(Console.ReadLine());
-        //    Console.WriteLine("Enter your DocumentNo: ");
-        //    newCustomer.DocumentNumber = Console.ReadLine();
 
 
-
-        //    using (AppDbContext context = new AppDbContext())
-        //    {
-        //        context.Add(newCustomer);
-        //        await context.SaveChangesAsync();
-        //    }
-        //}
+        public static async Task Read()
+        {
+            using (var alifcontext = new AppDbContext())
+            {
+                var customers = alifcontext.Customers;
+                await customers.ForEachAsync(customer => Console.WriteLine($"Id: {customer.Id}\t FirstName: {customer.FirstName}\t LastName: {customer.LastName}\t MiddleName: {customer.MidlleName}\t " +
+                                      $"Date of Birth: {customer.DateOfBirth}\t DocumentNumber: {customer.DocumentNumber}"));
+            }
+        }
     }
 
 }
